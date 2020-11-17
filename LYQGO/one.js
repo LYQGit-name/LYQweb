8436c47057e869f1a2903620b3062b2df624ee0b
你注册GitHub的用户名 <GitHub>
2020-11-17 10:10:25 +0800

    狗狗狗

---
+++ LYQGO/one.js
@@ -0,0 +1,9 @@
+function one() {
+    //var Let
+    var array = [12, 13, 124, 125, 126, 127,128];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+    }
+}
+one();
